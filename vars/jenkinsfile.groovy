79d82a948be687785b299ea877f9f2f590e5398d
muddushafi <rahman.java@gmail.com>
2023-01-09 23:36:56 -0500

    Update jenkinsfile.groovy

--- vars/jenkinsfile.groovy
+++ vars/jenkinsfile.groovy
@@ -1,13 +1,11 @@
 def call() {
   def template="pipeline-bls-jdk8"
   pipeline {
-    stages{
       stage{
         script {
           blsPipeline()
         } //script tag end
       } // stage tag end
-    }//stages tag end
   }//pipeline tag end
 } // call tag end
 
