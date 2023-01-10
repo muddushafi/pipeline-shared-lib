eed70745c91012402e54ba21565241c4ab9ec83c
muddushafi <rahman.java@gmail.com>
2023-01-10 00:45:24 -0500

    Made changes

--- vars/jenkinsfile.groovy
+++ vars/jenkinsfile.groovy
@@ -1,16 +1,19 @@
 def call() {
   def template="pipeline-bls-jdk8"
   pipeline {
-        stage("Build"){
-           step{
-                echo "Starting my pipeline"
-                script {
-                  echo "Entered script tag"
-                  blsPipeline()
-                  echo "Finished calling blspipeline"
-                } //script tag end
-            } // Steps tag end
-         }// stage tag end
+      agent any
+      stages {
+          stage('Build') {
+              steps {
+                  echo "Starting my pipeline"
+                  script {
+                      echo "Entered script tag"
+                      blsPipeline()
+                      echo "Finished calling blspipeline"
+                  }
+              }
+          }
+      }
   }//pipeline tag end
 } // call tag end
 
